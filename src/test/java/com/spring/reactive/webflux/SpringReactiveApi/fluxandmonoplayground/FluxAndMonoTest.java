5b4fc28605298a01d990af2a2ea0445641b69a3f
adityamanjrekar <aditya.manjrekar27@gmail.com>
2020-08-30 20:32:45 -0700

    Flux test for onError onComplete

--- src/test/java/com/spring/reactive/webflux/SpringReactiveApi/fluxandmonoplayground/FluxAndMonoTest.java
+++ src/test/java/com/spring/reactive/webflux/SpringReactiveApi/fluxandmonoplayground/FluxAndMonoTest.java
@@ -6,12 +6,14 @@ import reactor.core.publisher.Flux;
 public class FluxAndMonoTest {
 
     @Test
-    public void fluxTest(){
+    public void fluxTest() {
 
-      Flux<String> stringFlux =  Flux.just("Spring","Spring Boot","Reactive Spring")
-              .concatWith(Flux.error(new RuntimeException("Error occurred")))
-              .log();
-      stringFlux.subscribe(System.out::println,(e)-> System.err.println(e));
+        Flux<String> stringFlux = Flux.just("Spring", "Spring Boot", "Reactive Spring")
+                //.concatWith(Flux.error(new RuntimeException("Error occurred")))
+                .concatWith(Flux.just("After Error"))
+                .log();
+        stringFlux.subscribe(System.out::println, (e) -> System.err.println("Exception is " + e)
+                , () -> System.out.println("Completed"));
     }
 
 }
